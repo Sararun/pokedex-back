287b7b209de1393924bc620efcc11d1afe637b40
Sararun <sarosekvlad12@gmail.com>
2022-12-14 20:13:28 +0700

    Migrations

--- database/migrations/2022_12_13_043341_create_pokemon_table.php
+++ database/migrations/2022_12_13_043341_create_pokemon_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('pokemons', function (Blueprint $table) {
+        Schema::create('pokemon', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('image');
             $table->string('type');
+            $table->timestamps();
         });
     }
 
@@ -28,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::drop('pokemons');
+        Schema::dropIfExists('pokemon');
     }
 };
